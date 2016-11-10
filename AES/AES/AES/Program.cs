2100e05f78bb9f13c1339820caa0ac56b7fe8928
Kazyna <kazyna.abdrakhman@gmail.com>
2016-11-10 23:03:31 +0600

    AES project
    AES project on C#

---
+++ AES/AES/AES/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AES
+{
+
+    class Program
+    {
+        static string DecryptStringFromBytesAes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+      
+            string plaintext;
+
+            
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+             
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+        static byte[] EncryptStringToBytesAes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            return encrypted;
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string original = "Advanced Encryption Standard";
+
+                using (var myAes = Aes.Create())
+                {
+
+                    
+                    byte[] encrypted = EncryptStringToBytesAes(original, myAes.Key, myAes.IV);
+
+                    
+                    string roundtrip = DecryptStringFromBytesAes(encrypted, myAes.Key, myAes.IV);
+
+
+                    
+                    Console.WriteLine("Original:   {0}", original);
+                    Console.WriteLine("Encrypted:   {0}", encrypted);
+                   Console.WriteLine("Round Trip: {0}", roundtrip);
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+    }
+}
